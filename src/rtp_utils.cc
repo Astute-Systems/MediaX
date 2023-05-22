c8514a9285b4e5cc1b501d9c1d13168146d0158f
Ross Newman <ross.newman@defencex.com.au>
2023-05-22 10:37:27 +1000

    Adding Windows socket code

--- src/rtp_utils.cc
+++ src/rtp_utils.cc
@@ -22,7 +22,7 @@
 void EndianSwap32(uint32_t *data, unsigned int length) {
   // Check for Intel architecture
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-  for (uint32_t c = 0; c < length; c++) data[c] = __bswap_32(data[c]);
+  for (uint32_t c = 0; c < length; c++) data[c] = __bswapq(data[c]);
   return;
 // Check for ARM architecture
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
--- src/rtp_utils.h
+++ src/rtp_utils.h
@@ -14,23 +14,13 @@
 #ifndef RTP_UTILS_H
 #define RTP_UTILS_H
 
-#if _WIN32
+#ifdef _WIN32
 #include <winsock2.h>
 // Swap bytes in 16 bit value.
 #define __bswap_constant_16(x) ((((x) >> 8) & 0xffu) | (((x)&0xffu) << 8))
 // Swap bytes in 32 bit value.
-#define __bswap_constant_32(x) \
-  ((((x)&0xff000000u) >> 24) | (((x)&0x00ff0000u) >> 8) | (((x)&0x0000ff00u) << 8) | (((x)&0x000000ffu) << 24))
-#define __bswap_32(x)                  \
-  (__extension__({                     \
-    register unsigned int __bsx = (x); \
-    __bswap_constant_32(__bsx);        \
-  }))
-#define __bswap_16(x)               \
-  (__extension__({                  \
-    unsigned short int __bsx = (x); \
-    __bswap_constant_16(__bsx);     \
-  }))
+#define __bswap_constant_32(x) ((((x) >> 24) & 0xffu) | (((x)&0xff0000u) >> 8) | (((x)&0xff00u) << 8) | (((x)&0xffu) << 24))
+#define __bswap_16(x) __bswap_constant_16(x)
 #else
 #include <byteswap.h>
 #include <netdb.h>
@@ -38,6 +28,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #endif
+#include <stdint.h>
 
 void EndianSwap32(uint32_t *data, unsigned int length);
 void EndianSwap16(uint16_t *data, unsigned int length);
