4d9ac758852a046c0f92dac3eaad67f7205ad568
Ross Newman <enquiries@defencex.ai>
2023-07-21 08:09:14 +1000

    Fixing script issues

--- example/rtp-test.sh
+++ example/rtp-test.sh
@@ -7,7 +7,7 @@ IP_ADDRESS=127.0.0.1
 #===========================================
 
 echo 'Starting example application...'
-./build/bin/transmit-example -pattern=1 -ipaddr=$IP_ADDRESS -height=480 -width=640 -port=5004  &
+./build/bin/transmit-example -source=2 -ipaddr=$IP_ADDRESS -height=480 -width=640 -port=5004  &
 
 sleep 1
 
