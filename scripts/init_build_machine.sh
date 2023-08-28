5cc695b6e3ba5a7cafdf9bbf0d68deb11d654156
Ross Newman <enquiries@defencex.ai>
2023-08-28 15:31:28 +1000

    Fixing depends

--- scripts/init_build_machine.sh
+++ scripts/init_build_machine.sh
@@ -6,5 +6,5 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-apt-get --no-install-recommends -y install libswscale-dev libgflags-dev libgtkmm-3.0-dev cmake g++ libgtest-dev libva-dev libdrm-dev libx11-dev libxext-dev libxfixes-dev libvdpau-dev cppcheck cpplint libgoogle-glog-dev
+apt-get --no-install-recommends -y install libswscale-dev libgflags-dev libgtkmm-3.0-dev cmake g++ libgtest-dev libva-dev libdrm-dev libx11-dev libxext-dev libxfixes-dev libvdpau-dev cppcheck cpplint libunwind-dev libgoogle-glog-dev
 apt-get --no-install-recommends -y install libunwind-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
