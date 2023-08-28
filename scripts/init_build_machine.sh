0b07f537323143337c3f7dde47e067679a29c517
Ross Newman <enquiries@defencex.ai>
2023-08-28 18:19:43 +1000

    Adding gcov

--- scripts/init_build_machine.sh
+++ scripts/init_build_machine.sh
@@ -6,6 +6,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-apt-get --no-install-recommends -y install libswscale-dev libgflags-dev libgtkmm-3.0-dev cmake g++ libgtest-dev libva-dev libdrm-dev libx11-dev libxext-dev libxfixes-dev libvdpau-dev cppcheck cpplint libunwind-dev libgoogle-glog-dev lcov
+apt-get --no-install-recommends -y install libswscale-dev libgflags-dev libgtkmm-3.0-dev cmake g++ libgtest-dev libva-dev libdrm-dev libx11-dev libxext-dev libxfixes-dev libvdpau-dev cppcheck cpplint libunwind-dev libgoogle-glog-dev gcov lcov
 apt-get --no-install-recommends -y install libunwind-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-libav gstreamer1.0-tools
 # apt-get --no-install-recommends -y install libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
