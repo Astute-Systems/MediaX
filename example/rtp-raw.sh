10450e5cc3ec6ef027a1accae574a6c9f4ed9c1e
Ross Newman <enquiries@defencex.ai>
2023-09-12 10:49:11 +1000

    Renaming example scripts

---
+++ example/rtp-raw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#IP_ADDRESS=238.192.1.1
+IP_ADDRESS=127.0.0.1
+
+#Gstreamer1.0 working example UYVY streaming
+#===========================================
+
+echo 'Starting example application...'
+./build/bin/transmit-example -source=0 -mode=0 -ipaddr=$IP_ADDRESS -height=480 -width=640 -port=5004  &
+
+sleep 1
+
+echo 'Starting receiver...'
+
+./build/bin/receive-example -mode=0 -ipaddr=$IP_ADDRESS -height=480 -width=640 -port=5004  &
+
+sleep 10
+
+pkill -f transmit-example 
+pkill -f receive-example
+exit
+
+
+
+
+
