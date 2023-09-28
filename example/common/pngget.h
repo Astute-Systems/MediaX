8b466caa4b9d03b3f8fa6fad1d93e7b00a8233e0
Ross Newman <ross.newman@defencex.com.au>
2023-09-28 17:17:01 +1000

    Moving examples into seperate dirs

---
+++ example/common/pngget.h
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2023, DefenceX PTY LTD
+//
+// This file is part of the VivoeX project developed by DefenceX.
+//
+// Licensed under the Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
+// License. See the LICENSE file in the project root for full license details.
+//
+/// \brief a class to read a png file
+///
+/// \file pngget.h
+
+#ifndef PNGGET_H
+#define PNGGET_H
+
+#include <limits.h>
+#include <png.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+#include <vector>
+
+class Png {
+ public:
+  png_bytep* GetRowPointwes();
+  void ReadPngFile(char* file_name);
+  std::vector<uint8_t> ReadPngRgb24(std::string_view filename) const;
+
+ private:
+  png_structp png_ptr;
+
+  png_bytep* row_pointers;
+
+  int x;
+  int y;
+
+  int width;
+  int height;
+  png_byte color_type;
+  png_byte bit_depth;
+
+  png_infop info_ptr;
+  int number_of_passes;
+
+  void Abort(const char* s, ...);
+};
+
+#endif  // PNGGET_H
