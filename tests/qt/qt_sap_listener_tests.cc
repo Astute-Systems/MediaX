d8cd67405851fab2994d0f8453c18d46448b2e25
Ross Newman <enquiries@defencex.ai>
2023-10-08 18:11:02 +1000

    Adding tests for Qt SAP wrappers

--- tests/qt/qt_sap_listener_tests.cc
+++ tests/qt/qt_sap_listener_tests.cc
@@ -6,14 +6,22 @@
 using namespace mediax::qt;
 using namespace ::testing;
 
-TEST(QtSapListenerTest, StartTest) {
+TEST(QtSapListenerTest, Start) {
   mediax::qt::QtSapListener listener;
   listener.start();
+  listener.stop();
   // Add your assertions here to test the behavior of the start() method
 }
 
-TEST(QtSapListenerTest, StopTest) {
+TEST(QtSapListenerTest, Stop) {
   mediax::qt::QtSapListener listener;
+  listener.start();
+  listener.start();
+  listener.start();
+  listener.start();
+  listener.stop();
+  listener.stop();
+  listener.stop();
   listener.stop();
   // Add your assertions here to test the behavior of the stop() method
 }
