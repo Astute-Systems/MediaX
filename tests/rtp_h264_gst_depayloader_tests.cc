4a09b7c517d1a7bfdb4b9ce59277523d751287f4
Ross Newman <ross.newman@defencex.com.au>
2023-10-09 18:25:19 +1000

    Added extra H.264 test

--- tests/rtp_h264_gst_depayloader_tests.cc
+++ tests/rtp_h264_gst_depayloader_tests.cc
@@ -48,8 +48,6 @@ TEST(RtpH264DepayloaderTest, UnicastOk) {
   mediax::video::ColourSpaceCpu colourspace;
   mediax::h264::gst::vaapi::RtpH264Depayloader rtp;
 
-  // Increase GST debug level
-  setenv("GST_DEBUG", "3", 1);
   // Set the stream details individually
   rtp.SetIpAddress("127.0.0.1");
   rtp.SetPort(5004);
@@ -89,8 +87,6 @@ TEST(RtpH264DepayloaderTest, UnicastOkSetStreamInfo) {
   mediax::video::ColourSpaceCpu colourspace;
   mediax::h264::gst::vaapi::RtpH264Depayloader rtp;
 
-  // Increase GST debug level
-  setenv("GST_DEBUG", "3", 1);
   // Set the stream details using set stream info
   ::mediax::StreamInformation stream_info = {.session_name = "test_session_name",
                                              .hostname = "127.0.0.1",
@@ -122,4 +118,46 @@ TEST(RtpH264DepayloaderTest, UnicastOkSetStreamInfo) {
 
   std::cout << "Received" << std::endl;
   WritePngFile(rgb_test.data(), rtp.GetWidth(), rtp.GetHeight(), "H264_Image.png");
+}
+
+TEST(RtpH264DepayloaderTest, UnicastOkSetStreamInfoPtr) {
+#if !GST_SUPPORTED
+  GTEST_SKIP();
+#endif
+
+  std::array<uint8_t, 1280 * 720 * 3> rgb_test;
+  mediax::video::ColourSpaceCpu colourspace;
+  auto rtp = std::make_shared<mediax::h264::gst::vaapi::RtpH264Depayloader>();
+
+  // Set the stream details using set stream info
+  ::mediax::StreamInformation stream_info = {.session_name = "test_session_name",
+                                             .hostname = "127.0.0.1",
+                                             .port = 5004,
+                                             .height = 720,
+                                             .width = 1280,
+                                             .framerate = 25,
+                                             .encoding = ::mediax::ColourspaceType::kColourspaceH264Part10,
+                                             .deleted = false};
+  rtp->SetStreamInfo(stream_info);
+  std::cout << "Setup" << std::endl;
+
+  // Start the stream
+  EXPECT_TRUE(rtp->Open());
+  std::cout << "Opened" << std::endl;
+  rtp->Start();
+  std::cout << "Started" << std::endl;
+  uint8_t* data = rgb_test.data();
+  EXPECT_FALSE(rtp->Receive(&data, 80));
+  rtp->Stop();
+  rtp->Close();
+
+  EXPECT_EQ(rtp->GetColourSpace(), ::mediax::ColourspaceType::kColourspaceH264Part10);
+  mediax::video::ColourSpaceCpu convert;
+  convert.Nv12ToRgb(rtp->GetHeight(), rtp->GetWidth(), data, rgb_test.data());
+
+  EXPECT_EQ(rtp->GetHeight(), 720);
+  EXPECT_EQ(rtp->GetWidth(), 1280);
+
+  std::cout << "Received" << std::endl;
+  WritePngFile(rgb_test.data(), rtp->GetWidth(), rtp->GetHeight(), "H264_Image.png");
 }
