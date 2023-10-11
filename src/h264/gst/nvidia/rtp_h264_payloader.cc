21e6da23caace25a99fb796e0cda598448d8d91b
Ross Newman <ross.newman@defencex.com.au>
2023-10-11 17:09:10 +1000

    Added transmit scripted test

--- src/h264/gst/nvidia/rtp_h264_payloader.cc
+++ src/h264/gst/nvidia/rtp_h264_payloader.cc
@@ -44,8 +44,6 @@ GstFlowReturn SourceFrameCallback(GstAppSink *appsink, gpointer user_data) {
   GstMapInfo map;
   gst_buffer_map(buffer, &map, GST_MAP_READ);
 
-  // TODO(Ross Newman): Send video
-
   // Unmap the buffer
   gst_buffer_unmap(buffer, &map);
 
