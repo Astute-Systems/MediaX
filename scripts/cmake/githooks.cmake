6af712a253abc116e37df8ac840f4d33ebfceac6
Ross Newman <ross.newman@defencex.ai>
2023-12-03 12:21:00 +1000

    Fixing githooks

--- scripts/cmake/githooks.cmake
+++ scripts/cmake/githooks.cmake
@@ -2,12 +2,12 @@
 
 # if no aleady sym linked
 execute_process (
-    COMMAND cp  ${CMAKE_SOURCE_DIR}/.githooks/pre-push ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push
-    COMMAND cp  ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-doxygen ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-doxygen
-    COMMAND cp  ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-linter ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-linter
-    COMMAND cp  ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-todo ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-todo
-    COMMAND cp  ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-untracked ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-untracked
-    COMMAND cp  ${CMAKE_SOURCE_DIR}/.githooks/pre-push-conflict-check ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-conflict-check
+    COMMAND cp -f ${CMAKE_SOURCE_DIR}/.githooks/pre-push ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push
+    COMMAND cp -f ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-doxygen ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-doxygen
+    COMMAND cp -f ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-linter ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-linter
+    COMMAND cp -f ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-todo ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-todo
+    COMMAND cp -f ${CMAKE_SOURCE_DIR}/.githooks/pre-push-check-untracked ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-check-untracked
+    COMMAND cp -f ${CMAKE_SOURCE_DIR}/.githooks/pre-push-conflict-check ${CMAKE_SOURCE_DIR}/.git/hooks/pre-push-conflict-check
     COMMAND git config core.hooksPath .git/hooks
     OUTPUT_VARIABLE outVar
 
