e6553d6d552f65441a39e99da457b9063a5cca5b
Ross Newman <ross.newman@defencex.ai>
2023-12-03 13:12:24 +1000

    Fixing version number embedded into doxygen

--- scripts/cmake/doxygen.cmake
+++ scripts/cmake/doxygen.cmake
@@ -5,5 +5,5 @@ add_custom_command(
     COMMAND mkdir -p ${CMAKE_BINARY_DIR}/images
     COMMAND cd ${CMAKE_BINARY_DIR} && cmake .. --graphviz=depends.dot && dot -Tpng  depends.dot.mediax -o ${CMAKE_BINARY_DIR}/images/mediax.png
     # COMMAND cd ${CMAKE_BINARY_DIR} && dot -Tpng  depends.dot.tests -o ${CMAKE_BINARY_DIR}/images/test_depends.png
-    COMMAND GIT_HASH=${GIT_HASH} CMAKE_MAJOR_VERSION=${CMAKE_MAJOR_VERSION} CMAKE_MINOR_VERSION=${CMAKE_MINOR_VERSION} CMAKE_PATCH_VERSION=${CMAKE_PATCH_VERSION} CMAKE_BINARY_DIR=${CMAKE_BINARY_DIR} doxygen
+    COMMAND GIT_HASH=${GIT_HASH} SEM_VERSION=${MEDIAX_VERSION}${MEDIAX_SUFFIX} doxygen
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
