6d7d6fee9c22808a0f4297f71969229e3ef49ca3
Ross Newman <ross.newman@defencex.ai>
2023-12-04 10:33:16 +1000

    Fixing incorrect example directory names

--- scripts/doxygencheck.sh
+++ scripts/doxygencheck.sh
@@ -4,7 +4,13 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 LIMIT=129
 WARNINGS=$(doxygen |&  grep "warning" | wc -l)
+# If no warnings, exit with 0
+if [ "$WARNINGS" -eq 0 ]; then
+    echo -e "${GREEN}No DOXYGEN warnings${NC}"
+    exit 0
+fi
 echo -e "Doxygen warnings: ${GREEN}$WARNINGS${NC}"
+
 if [ "$WARNINGS" -gt "$LIMIT" ]; then
     echo -e "Doxygen warnings exceed ${RED}$LIMIT${NC}, failing build."
     exit $WARNINGS
