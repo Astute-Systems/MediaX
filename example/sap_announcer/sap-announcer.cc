657cf92b88992a2466502a16f2d4eee127e19d68
Ross Newman <ross.newman@defencex.ai>
2023-12-05 07:41:14 +0000

    SAP announcer now setup for Pi Cameras

--- example/sap_announcer/sap-announcer.cc
+++ example/sap_announcer/sap-announcer.cc
@@ -40,58 +40,26 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 
   // Add all your stream announcments here
   sap.AddSapAnnouncement(
-      {"Stream 1", "239.192.1.1", 5000, 480, 640, 25, mediax::rtp::ColourspaceType::kColourspaceYuv, false});
+      {"Pi Camera 1", "239.192.1.1", 5004, 720, 1280, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
   sap.AddSapAnnouncement(
-      {"Stream 2", "239.192.1.2", 6000, 800, 600, 30, mediax::rtp::ColourspaceType::kColourspaceYuv, false});
+      {"Pi Camera 2", "239.192.2.1", 5004, 720, 1280, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
   sap.AddSapAnnouncement(
-      {"Stream 3", "239.192.1.3", 7000, 1920, 1024, 60, mediax::rtp::ColourspaceType::kColourspaceYuv, false});
+      {"Pi Camera 3", "239.192.3.1", 5004, 720, 1280, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
   sap.AddSapAnnouncement(
-      {"Stream 4", "239.192.1.4", 8000, 1920, 1024, 60, mediax::rtp::ColourspaceType::kColourspaceRgb24, false});
+      {"Pi Camera 4", "239.192.4.1", 5004, 720, 1280, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
   sap.AddSapAnnouncement(
-      {"Stream 5", "239.192.1.5", 9000, 1920, 1024, 60, mediax::rtp::ColourspaceType::kColourspaceMono8, false});
+      {"Pi Camera 5", "239.192.5.1", 5004, 720, 1280, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
   sap.AddSapAnnouncement(
-      {"Stream 6", "239.192.1.6", 10000, 1920, 1024, 60, mediax::rtp::ColourspaceType::kColourspaceJpeg2000, false});
-  sap.AddSapAnnouncement(
-      {"Stream 7", "239.192.1.7", 20000, 1920, 1024, 60, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
+      {"Pi Camera 6", "239.192.6.1", 5004, 720, 1280, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
   // Start all the streams
   sap.Start();
-  // [Sap example announcer]
-
-  // Wait 10 seconds
-  sleep(10);
-  sap.Stop();
-
-  // Thread is running but all streams stopped at this point for 2 seconds
-  sleep(2);
-
-  // Re-start all the streams for 5 seconds
-  sap.Start();
-  sleep(5);
-  // [Sap example stop]
-
-  sap.DeleteAllSapAnnouncements();
-  // Kill the SAP announcer, can be re-started by calling Start(); again
-  sap.Stop();
-  // [Sap example stop]
-
-  sap.Start();
-  sap.AddSapAnnouncement(
-      {"Stream 10", "239.192.1.250", 5004, 480, 640, 25, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
-  sap.AddSapAnnouncement(
-      {"Stream 11", "239.192.1.252", 5004, 800, 600, 30, mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
-  sap.AddSapAnnouncement({"Stream 12", "239.192.3.252", 5004, 1920, 1024, 60,
-                          mediax::rtp::ColourspaceType::kColourspaceH264Part10, false});
-
-  sleep(5);
-
-  sap.DeleteSapAnnouncement("Stream 11");
 
   // Loop till SIGINT
   while (!stop_signal) {
     sleep(1);
   }
 
-  // Should be two live stream now, run till CTL+C
+  sap.Stop();
 
   return 0;
 }
