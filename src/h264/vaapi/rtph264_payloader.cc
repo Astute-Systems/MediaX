3af6fd706b3daa2fa6619d71007b6afccec52b5c
Toby Newman <toby.newman@defencex.ai>
2024-01-09 11:38:05 +1000

    Updated the year

--- src/h264/vaapi/rtph264_payloader.cc
+++ src/h264/vaapi/rtph264_payloader.cc
@@ -1,6 +1,6 @@
 
 //
-// Copyright (c) 2023, DefenceX PTY LTD
+// Copyright (c) 2024, DefenceX PTY LTD
 //
 // This file is part of the VivoeX project developed by DefenceX.
 //
--- src/utils/colourspace_cpu.cc
+++ src/utils/colourspace_cpu.cc
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2023, DefenceX PTY LTD
+// Copyright (c) 2024, DefenceX PTY LTD
 //
 // This file is part of the VivoeX project developed by DefenceX.
 //
