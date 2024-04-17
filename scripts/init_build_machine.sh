8be3c5bf1056dfac8f78eec9360fa203300befe5
Ross Newman <ross.newman@defencex.ai>
2024-04-17 15:25:06 +1000

    Fixing tokens for Ununtu 24.04

--- scripts/init_build_machine.sh
+++ scripts/init_build_machine.sh
@@ -15,7 +15,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Basic setup for empty docker containers
-apt-get --no-install-recommends -y install cmake make gcc g++ git python3-dev
+apt-get --no-install-recommends -y install cmake make gcc g++ git python3-dev tar zip
 # Library dependencies
 apt-get --no-install-recommends -y install libv4l-dev libswscale-dev libgflags-dev libgmock-dev libgtkmm-3.0-dev cmake g++ libgtest-dev libva-dev libdrm-dev libx11-dev libxext-dev libxfixes-dev libvdpau-dev cppcheck libunwind-dev libgoogle-glog-dev gcovr lcov swig
 apt-get --no-install-recommends -y install libunwind-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-libav gstreamer1.0-tools
